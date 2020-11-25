96b2ad8a0db87e228da9ae82af27d033e5834724
AUJacobNyvang <70698246+AUJacobNyvang@users.noreply.github.com>
2020-11-25 14:08:42 +0100

    Integration step 4, to Start og stop testet

--- MicrowaveOvenCore/Microwave.Test.Integration/IntegrationTestStep4.cs
+++ MicrowaveOvenCore/Microwave.Test.Integration/IntegrationTestStep4.cs
@@ -1,7 +1,58 @@
-﻿namespace Microwave.Test.Integration
+﻿using System;
+using System.IO;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
 {
+    [TestFixture]
     public class IntegrationTestStep4
     {
-        
+        private StringWriter str;
+        private IOutput output;
+        private IDisplay displayUT;
+        private IPowerTube powerTubeUT;
+        private ITimer TimerUT;
+        private ICookController CookControllerUT;
+        private IUserInterface userinterfaceSUB;
+        private IButton ButtonSUB;
+        private IDoor DoorSUB;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            str = new StringWriter();
+            Console.SetOut(str);
+            output = new Output();
+            displayUT = new Display(output);
+            powerTubeUT = new PowerTube(output);
+            TimerUT = new Timer();
+
+            userinterfaceSUB = Substitute.For<IUserInterface>(default);
+            CookControllerUT = new CookController(TimerUT,displayUT,powerTubeUT,userinterfaceSUB);
+        }
+
+        [TestCase(1, 40)]
+        [TestCase(100, 10)]
+        public void CookController_StartCookingCalled_PowertubeStarts(int power, int time)
+        {
+            CookControllerUT.StartCooking(power, time);
+
+            Assert.That(str.ToString().Contains($"PowerTube works with {power}"));
+        }
+
+        [Test]
+        public void CookController_stopCalled_PowertubeStops()
+        {
+            CookControllerUT.StartCooking(50,10);
+
+            CookControllerUT.Stop();
+            Assert.That(str.ToString().Contains($"PowerTube turned off"));
+        }
+
     }
 }
