0bb85c299f593ee8c976b922b4a09054ee673da3
AUJacobNyvang <70698246+AUJacobNyvang@users.noreply.github.com>
2020-11-26 10:14:30 +0100

    Mere test til step 4

--- MicrowaveOvenCore/Microwave.Test.Integration/IntegrationTestStep4.cs
+++ MicrowaveOvenCore/Microwave.Test.Integration/IntegrationTestStep4.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using Microwave.Classes.Boundary;
 using Microwave.Classes.Controllers;
@@ -36,21 +37,54 @@ namespace Microwave.Test.Integration
             CookControllerUT = new CookController(TimerUT,displayUT,powerTubeUT,userinterfaceSUB);
         }
 
+        [TestCase(50, 40)]
         [TestCase(1, 40)]
-        [TestCase(100, 10)]
+        [TestCase(100, 40)]
         public void CookController_StartCookingCalled_PowertubeStarts(int power, int time)
         {
+            //Arrange
+
+            //Act
             CookControllerUT.StartCooking(power, time);
 
+            //Assert
             Assert.That(str.ToString().Contains($"PowerTube works with {power}"));
+        }
+
+        [TestCase(0, 40)]
+        [TestCase(101, 40)]
+        public void CookController_StartCookingCalled_InvalidPowertubeValues(int power, int time)
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(delegate { CookControllerUT.StartCooking(power,time); });
+
+        }
+
+        [Test]
+        public void CookController_StartCooking_WhileCookControllerIsAlreadyCooking()
+        {
+            //Arrange
+            int power = 50;
+            int time = 40;
+            CookControllerUT.StartCooking(power, time);
+            //Act
+
+            //Assert
+            Assert.Throws<ApplicationException>(delegate { CookControllerUT.StartCooking(power, time); });
         }
 
         [Test]
         public void CookController_stopCalled_PowertubeStops()
         {
+            //Arrange
             CookControllerUT.StartCooking(50,10);
-
+            //Act
             CookControllerUT.Stop();
+            //Assert
             Assert.That(str.ToString().Contains($"PowerTube turned off"));
         }
 
